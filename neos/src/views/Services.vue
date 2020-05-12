d08b51113d75916a25a88d75685d166afe7d6373
unknown <kaleemullahlqp@gmail.com>
2020-05-12 02:20:11 -0700

    footer added

--- neos/src/views/Services.vue
+++ neos/src/views/Services.vue
@@ -152,81 +152,7 @@
       </div>
     </div>
 
-    <footer class="site-footer bg-dark">
-      <div class="container">
-        
-
-        <div class="row">
-          <div class="col-md-4 mb-4 mb-md-0">
-            <h3 class="footer-heading mb-4 text-white">About</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat quos rem ullam, placeat amet.</p>
-            <p><a href="#" class="btn btn-primary text-white px-4">Read More</a></p>
-          </div>
-          <div class="col-md-5 mb-4 mb-md-0 ml-auto">
-            <div class="row mb-4">
-              <div class="col-md-6">
-                <h3 class="footer-heading mb-4 text-white">Quick Menu</h3>
-                  <ul class="list-unstyled">
-                    <li><router-link to="/">Home</router-link></li>
-                    <li><router-link to="/about">About</router-link></li>
-                    <li><router-link to="/blog">Blog</router-link></li>
-                    <li><router-link to="/contact">Contact</router-link></li>
-                    
-                  </ul>
-              </div>
-              <div class="col-md-6">
-                <h3 class="footer-heading mb-4 text-white">Free Templates</h3>
-                  <ul class="list-unstyled">
-                    <li><a href="#">HTML5 / CSS3</a></li>
-                    <li><a href="#">Clean Design</a></li>
-                    <li><a href="#">Responsive</a></li>
-                    <li><a href="#">Multi Purpose Template</a></li>
-                  </ul>
-              </div>
-            </div>
-
-            <div class="row mb-5">
-              <div class="col-md-12">
-              <h3 class="footer-heading mb-4 text-white">Stay up to date</h3>
-              <form action="#" class="d-flex footer-subscribe">
-                <input type="text" class="form-control rounded-0" placeholder="Enter your email">
-                <input type="submit" class="btn btn-primary rounded-0" value="Subscribe">
-              </form>
-            </div>
-            </div>
-          </div>
-
-          
-          <div class="col-md-2">
-            
-            <div class="row">
-            <div class="col-md-12"><h3 class="footer-heading mb-4 text-white">Social Icons</h3></div>
-              <div class="col-md-12">
-                <p>
-                  <a href="#" class="pb-2 pr-2 pl-0"><span class="icon-facebook"></span></a>
-                  <a href="#" class="p-2"><span class="icon-twitter"></span></a>
-                  <a href="#" class="p-2"><span class="icon-instagram"></span></a>
-                  <a href="#" class="p-2"><span class="icon-vimeo"></span></a>
-
-                </p>
-              </div>
-          </div>
-          </div>
-        </div>
-        <div class="row pt-5 mt-5 text-center">
-          <div class="col-md-12">
-            <p>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            Copyright &copy; 2020 All rights reserved | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            </p>
-
-            
-          </div>
-          
-        </div>
-      </div>
-    </footer>
+    <Footer></Footer>
 
   </div>
   </div>
@@ -235,11 +161,13 @@
 <script>
 // @ is an alias to /src
 import Navigation from "@/components/Navigation.vue";
+import Footer from "@/components/Footer.vue";
 
 export default {
   name: "Services",
   components: {
     Navigation,
+    Footer
   },
   data: function(){
     return {
